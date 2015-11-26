c25ae65f9f8c863e2c5eece27e805cd49b6e707a
Brad Roberts <email@bradroberts.net>
2015-11-25 19:55:08 -0500

    Refactoring

--- app.js
+++ app.js
@@ -15,7 +15,7 @@
 
         requests: {
 
-            getTasks: function (ticket_id) {
+            getTasksByTicket: function (ticket_id) {
                 return {
                     url:      'http://admin.faithpromise.192.168.10.10.xip.io/api/tickets/' + ticket_id + '/tasks',
                     type:     'GET',
@@ -58,7 +58,7 @@
         },
 
         // Nav bar
-        on_pane_activated: function() {
+        on_pane_activated: function () {
 
             if (this.currentLocation() === 'nav_bar') {
                 this.switchTo('calendar');
@@ -72,52 +72,76 @@
             this.fp = {};
 
             if (this.currentLocation() === 'ticket_sidebar' && event.firstLoad) {
-                this.load_tasks();
+                this.load_ticket_tasks();
             }
 
         },
 
-        load_tasks: function () {
+        load_ticket_tasks: function () {
+
             var ticket_id = this.ticket().id();
 
-            this.ajax('getTasks', ticket_id).done(function (data) {
-                this.fp.tasks          = data.tasks;
+            this.ajax('getTasksByTicket', ticket_id).done(function (data) {
+                this.fp.tasks          = data.tasks; // TODO: needs to be different than calendar tasks
                 this.format_task_dates(data.tasks);
                 data.no_tasks_found    = data.tasks.length === 0;
                 data.has_overdue_tasks = this._has_overdue_tasks();
-                data.tasks             = this.split_tasks(data.tasks);
+                data.tasks             = this.split_ticket_tasks(data.tasks);
                 this.switchTo('tasks', data);
             });
+
         },
 
-        mark_task_complete: function (event) {
+        load_calendar: function() {
+            alert('load calendar');
+        },
 
-            var ticket_id = this.ticket().id(),
-                task_id   = this.$(event.target).data('task-id'),
-                agent     = this.currentUser(),
-                data      = {
+        mark_task_complete: function (event, ticket_id) {
+
+            var is_sidebar = this.currentLocation() === 'ticket_sidebar',
+                is_calendar = this.currentLocation() === 'nav_bar',
+                task_id = this.$(event.target).data('task-id'),
+                agent   = this.currentUser(),
+                data    = {
                     completed_at:       moment().format(),
                     completed_by_email: agent.email(),
                     completed_by_name:  agent.name()
                 };
 
+            if (is_sidebar) {
+                ticket_id = this.ticket().id();
+            }
+
             this.ajax('update', ticket_id, task_id, data).done(function () {
-                this.load_tasks();
+
+                if (is_sidebar) {
+                    this.load_ticket_tasks();
+                }
+
+                if (is_calendar) {
+                    this.load_calendar();
+                }
+
             });
 
         },
 
-        mark_task_incomplete: function (event) {
+        mark_task_incomplete: function (event, ticket_id) {
 
-            var $target    = this.$(event.target),
-                ticket_id  = this.ticket().id(),
+            var is_sidebar = this.currentLocation() === 'ticket_sidebar',
+                is_calendar = this.currentLocation() === 'nav_bar',
+                $target    = this.$(event.target),
                 task_id    = $target.data('task-id'),
-                task_title = this._get_task_title(task_id),
+                task_title = $target.data('task-title'),
                 data       = {
                     completed_at:       null,
                     completed_by_email: null,
                     completed_by_name:  null
                 };
+
+            if (is_sidebar) {
+                ticket_id = this.ticket().id();
+            }
 
             // Checkbox has no meaning. It's just a visual. So, if it stays
             // in incomplete, make sure it's always checked.
@@ -125,19 +149,40 @@
 
             if (confirm('Are you sure you want to mark the task,\n"' + task_title + '" incomplete?')) {
                 this.ajax('update', ticket_id, task_id, data).done(function () {
-                    this.load_tasks();
+
+                    if (is_sidebar) {
+                        this.load_ticket_tasks();
+                    }
+
+                    if (is_calendar) {
+                        this.load_calendar();
+                    }
+
                 });
             }
 
         },
 
-        delete_task: function (event) {
+        delete_task: function (event, ticket_id) {
 
-            var ticket_id = this.ticket().id(),
+            var is_sidebar = this.currentLocation() === 'ticket_sidebar',
+                is_calendar = this.currentLocation() === 'nav_bar',
                 task_id   = this.$(event.target).data('task-id');
 
+            if (is_sidebar) {
+                ticket_id = this.ticket().id();
+            }
+
             this.ajax('delete', ticket_id, task_id).done(function () {
-                this.load_tasks();
+
+                if (is_sidebar) {
+                    this.load_ticket_tasks();
+                }
+
+                if (is_calendar) {
+                    this.load_calendar();
+                }
+
             });
 
         },
@@ -172,14 +217,16 @@
             event.preventDefault();
 
             this.ajax('save', ticket_id, data).done(function () {
+
                 services.notify('New task added', 'notice');
                 this.show_task_form();
+
             });
 
         },
 
         cancel_task_form: function () {
-            this.load_tasks();
+            this.load_ticket_tasks();
         },
 
         format_task_dates: function (tasks) {
@@ -203,7 +250,7 @@
 
         },
 
-        split_tasks: function (tasks) {
+        split_ticket_tasks: function (tasks) {
 
             var i, now = moment(), is_overdue, result = {incomplete: [], complete: []};
 
@@ -226,18 +273,6 @@
             }
 
             return result;
-        },
-
-        _get_task_title: function (id) {
-
-            var i;
-
-            for (i = 0; i < this.fp.tasks.length; i++) {
-                if (this.fp.tasks[i].id == id) {
-                    return this.fp.tasks[i].title;
-                }
-            }
-            return 'Unknown';
         },
 
         _has_overdue_tasks: function () {
