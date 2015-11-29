9e60ccfcbd2e6f2d957a73b2e5ae5da64edc44cc
Brad Roberts <email@bradroberts.net>
2015-11-29 10:16:25 -0500

    Refactoring and adding calendar loading

--- app.js
+++ app.js
@@ -15,17 +15,38 @@
 
         requests: {
 
-            getTasksByTicket: function (ticket_id) {
+            get_all_tickets: function () {
+                return {
+                    url:      '/api/v2/search.json?query=type:ticket%20status<solved',
+                    dataType: 'json'
+                }
+            },
+
+            get_my_tickets: function () {
+                return {
+                    url:      '/api/v2/search.json?query=type:ticket%20assignee:me%20status<solved',
+                    dataType: 'json'
+                }
+            },
+
+            get_tasks: function (ticket_ids) {
+                return {
+                    url:      'http://admin.faithpromise.192.168.10.10.xip.io/api/tasks',
+                    data:     { zendesk_ticket_ids: ticket_ids },
+                    dataType: 'json',
+                    cors:     true
+                };
+            },
+
+            get_ticket_tasks: function (ticket_id) {
                 return {
                     url:      'http://admin.faithpromise.192.168.10.10.xip.io/api/tickets/' + ticket_id + '/tasks',
-                    type:     'GET',
                     dataType: 'json',
                     cors:     true
                 };
             },
 
             save: function (ticket_id, data) {
-
                 return {
                     url:      'http://admin.faithpromise.192.168.10.10.xip.io/api/tickets/' + ticket_id + '/tasks',
                     type:     'POST',
@@ -36,7 +57,6 @@
             },
 
             update: function (ticket_id, task_id, data) {
-
                 return {
                     url:      'http://admin.faithpromise.192.168.10.10.xip.io/api/tickets/' + ticket_id + '/tasks/' + task_id,
                     type:     'PATCH',
@@ -47,7 +67,6 @@
             },
 
             delete: function (ticket_id, task_id) {
-
                 return {
                     url:  'http://admin.faithpromise.192.168.10.10.xip.io/api/tickets/' + ticket_id + '/tasks/' + task_id,
                     type: 'DELETE',
@@ -57,52 +76,89 @@
 
         },
 
-        // Nav bar
-        on_pane_activated: function () {
+        on_app_activated: function (event) {
 
-            if (this.currentLocation() === 'nav_bar') {
-                this.switchTo('calendar');
+            this.fp = this.fp || {};
+
+            if (event.firstLoad) {
+                this.load_ticket_tasks(event);
             }
 
         },
 
-        // Ticket sidebar
-        on_app_activated: function (event) {
-
-            this.fp = {};
-
-            if (this.currentLocation() === 'ticket_sidebar' && event.firstLoad) {
-                this.load_ticket_tasks();
-            }
-
+        on_pane_activated: function () {
+            this.load_calendar();
         },
 
         load_ticket_tasks: function () {
 
+            if (this.currentLocation() !== 'ticket_sidebar')
+                return;
+
             var ticket_id = this.ticket().id();
 
-            this.ajax('getTasksByTicket', ticket_id).done(function (data) {
-                this.fp.tasks          = data.tasks; // TODO: needs to be different than calendar tasks
+            this.ajax('get_ticket_tasks', ticket_id).done(function (data) {
+
                 this.format_task_dates(data.tasks);
+
                 data.no_tasks_found    = data.tasks.length === 0;
-                data.has_overdue_tasks = this._has_overdue_tasks();
+                data.has_overdue_tasks = this._has_overdue_tasks(data.tasks);
                 data.tasks             = this.split_ticket_tasks(data.tasks);
+
                 this.switchTo('tasks', data);
             });
 
         },
 
-        load_calendar: function() {
-            alert('load calendar');
+        load_calendar: function () {
+
+            if (this.currentLocation() !== 'nav_bar')
+                return;
+
+            var self     = this,
+                agent_id = this.currentUser().id(),
+                ticket_ref;
+
+            self.ajax('get_my_tickets', agent_id).done(function (data) {
+
+                ticket_ref = self.build_ticket_reference(data.results);
+
+                self.ajax('get_tasks', Object.keys(ticket_ref).join(',')).done(function (data) {
+
+                    this.format_task_dates(data.tasks);
+
+                    data.no_tasks_found = data.tasks.length === 0;
+                    data.categories     = this.split_calendar_tasks(data.tasks, ticket_ref);
+
+                    this.switchTo('calendar', data);
+
+                    ticket_ref = null;
+
+                });
+
+            });
+
+        },
+
+        build_ticket_reference: function (tickets) {
+
+            var i, ref = {};
+
+            for (i = 0; i < tickets.length; i++) {
+                ref[tickets[i].id] = tickets[i].subject;
+            }
+
+            return ref;
+
         },
 
         mark_task_complete: function (event, ticket_id) {
 
-            var is_sidebar = this.currentLocation() === 'ticket_sidebar',
+            var is_sidebar  = this.currentLocation() === 'ticket_sidebar',
                 is_calendar = this.currentLocation() === 'nav_bar',
-                task_id = this.$(event.target).data('task-id'),
-                agent   = this.currentUser(),
-                data    = {
+                task_id     = this.$(event.target).data('task-id'),
+                agent       = this.currentUser(),
+                data        = {
                     completed_at:       moment().format(),
                     completed_by_email: agent.email(),
                     completed_by_name:  agent.name()
@@ -128,12 +184,12 @@
 
         mark_task_incomplete: function (event, ticket_id) {
 
-            var is_sidebar = this.currentLocation() === 'ticket_sidebar',
+            var is_sidebar  = this.currentLocation() === 'ticket_sidebar',
                 is_calendar = this.currentLocation() === 'nav_bar',
-                $target    = this.$(event.target),
-                task_id    = $target.data('task-id'),
-                task_title = $target.data('task-title'),
-                data       = {
+                $target     = this.$(event.target),
+                task_id     = $target.data('task-id'),
+                task_title  = $target.data('task-title'),
+                data        = {
                     completed_at:       null,
                     completed_by_email: null,
                     completed_by_name:  null
@@ -165,9 +221,9 @@
 
         delete_task: function (event, ticket_id) {
 
-            var is_sidebar = this.currentLocation() === 'ticket_sidebar',
+            var is_sidebar  = this.currentLocation() === 'ticket_sidebar',
                 is_calendar = this.currentLocation() === 'nav_bar',
-                task_id   = this.$(event.target).data('task-id');
+                task_id     = this.$(event.target).data('task-id');
 
             if (is_sidebar) {
                 ticket_id = this.ticket().id();
@@ -252,7 +308,7 @@
 
         split_ticket_tasks: function (tasks) {
 
-            var i, now = moment(), is_overdue, result = {incomplete: [], complete: []};
+            var i, now = moment(), is_overdue, result = { incomplete: [], complete: [] };
 
             for (i = 0; i < tasks.length; i++) {
 
@@ -275,12 +331,62 @@
             return result;
         },
 
-        _has_overdue_tasks: function () {
+        split_calendar_tasks: function (tasks, ticket_ref) {
+
+            var i,
+                now            = moment(),
+                soon_before    = moment().add(7, 'days'),
+                temp_date,
+                is_overdue,
+                is_today,
+                is_soon,
+                today_tasks    = [],
+                soon_tasks     = [],
+                future_tasks   = [],
+                complete_tasks = [];
+
+            for (i = 0; i < tasks.length; i++) {
+
+                temp_date = moment(tasks[i].due_at);
+
+                is_overdue = temp_date.isBefore(now);
+                is_today   = temp_date.isSame(now, 'day');
+                is_soon    = !is_today && !is_overdue && temp_date.isBefore(soon_before);
+
+                tasks[i].subject = ticket_ref[tasks[i].zendesk_ticket_id];
+
+                if (tasks[i].completed_at) {
+                    tasks[i].status = 'complete';
+                    complete_tasks.push(tasks[i]);
+
+                } else if (is_overdue || is_today) {
+                    tasks[i].status = is_overdue ? 'overdue' : 'incomplete';
+                    today_tasks.push(tasks[i]);
+
+                } else if (is_soon) {
+                    tasks[i].status = 'incomplete';
+                    soon_tasks.push(tasks[i]);
+
+                } else {
+                    tasks[i].status = 'incomplete';
+                    future_tasks.push(tasks[i]);
+                }
+            }
+
+            return [
+                { title: 'Today', tasks: today_tasks, count: today_tasks.length },
+                { title: 'Next 7 Days', tasks: soon_tasks, count: soon_tasks.length },
+                { title: 'Future', tasks: future_tasks, count: future_tasks.length },
+                { title: 'Completed', tasks: complete_tasks, count: complete_tasks.length }
+            ];
+        },
+
+        _has_overdue_tasks: function (tasks) {
 
             var i, now = moment();
 
-            for (i = 0; i < this.fp.tasks.length; i++) {
-                if (!this.fp.tasks[i].completed_at && moment(this.fp.tasks[i].due_at).isBefore(now)) {
+            for (i = 0; i < tasks.length; i++) {
+                if (!tasks[i].completed_at && moment(tasks[i].due_at).isBefore(now)) {
                     return true;
                 }
             }
